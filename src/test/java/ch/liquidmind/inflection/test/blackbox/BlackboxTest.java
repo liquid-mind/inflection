005b1c09f5c0898634a1fdc6fef354ba0ba6c120
Adrian Moser <trekking09@gmail.com>
2015-11-11 14:15:31 +0100

    organize imports

--- src/test/java/ch/liquidmind/inflection/test/blackbox/BlackboxTest.java
+++ src/test/java/ch/liquidmind/inflection/test/blackbox/BlackboxTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertNotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import ch.liquidmind.inflection.proxy.ProxyRegistry;
