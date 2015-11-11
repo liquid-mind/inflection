f95bebe8f05b382898c8c32c54151b338105cec7
Adrian Moser <trekking09@gmail.com>
2015-11-11 15:32:48 +0100

    tests: more compiler tests added

---
+++ src/test/java/ch/liquidmind/inflection/compiler/CompilerTestUtility.java
@@ -0,0 +1,24 @@
+package ch.liquidmind.inflection.compiler;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.io.Files;
+
+import ch.liquidmind.inflection.compiler.CompilationJob.CompilationMode;
+import ch.liquidmind.inflection.loader.TaxonomyLoader;
+
+public final class CompilerTestUtility {
+
+	public static CompilationJob createCompilationJob(CompilationMode mode, URL... urls) throws URISyntaxException {
+		List<File> files = new ArrayList<>();
+		for (URL url : urls) {
+			files.add(new File(url.toURI()));
+		}
+		return new CompilationJob(TaxonomyLoader.getSystemTaxonomyLoader(), Files.createTempDir(), mode, files.toArray(new File [0]));
+	}
+	
+}
--- src/test/java/ch/liquidmind/inflection/compiler/InflectionCompilerTest.java
+++ src/test/java/ch/liquidmind/inflection/compiler/InflectionCompilerTest.java
@@ -4,24 +4,17 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.net.URL;
-
 import org.junit.Ignore;
 import org.junit.Test;
 
-import com.google.common.io.Files;
-
 import ch.liquidmind.inflection.compiler.CompilationJob.CompilationMode;
-import ch.liquidmind.inflection.loader.TaxonomyLoader;
 
 public class InflectionCompilerTest {
 
 	@Test
 	public void testCompile() throws Exception {
-		URL url = this.getClass().getResource("CompilerTestModel.inflect");
-		File file = new File(url.toURI());
-		CompilationJob job = new CompilationJob(TaxonomyLoader.getSystemTaxonomyLoader(), Files.createTempDir(), CompilationMode.NORMAL, file);
+		CompilationJob job = CompilerTestUtility.createCompilationJob(CompilationMode.NORMAL, 
+				this.getClass().getResource("CompilerTestModel.inflect"));
 		InflectionCompiler.compile(job);
 		assertFalse(job.getCompilationUnits().isEmpty());
 	}
@@ -29,14 +22,32 @@ public class InflectionCompilerTest {
 	@Test
 	@Ignore("IndexOutOfBoundsException is thrown")
 	public void testMessage() throws Exception {
-		URL url = this.getClass().getResource("CompilerTestModel.inflect");
-		File file = new File(url.toURI());
-		CompilationJob job = new CompilationJob(TaxonomyLoader.getSystemTaxonomyLoader(), Files.createTempDir(), CompilationMode.BOOTSTRAP, file);
+		CompilationJob job = CompilerTestUtility.createCompilationJob(CompilationMode.BOOTSTRAP, 
+				this.getClass().getResource("CompilerTestModel.inflect"));
 		InflectionCompiler.compile(job);
 		assertFalse(job.getCompilationFaults().isEmpty());
 		String message = job.getCompilationFaults().get(0).createFaultMessage();
 		assertNotNull(message != null);
 		assertTrue(message.length() > 0);
+	}
+	
+	@Test
+	@Ignore("Should not compile")
+	public void testInheritance1() throws Exception {
+		CompilationJob job = CompilerTestUtility.createCompilationJob(CompilationMode.BOOTSTRAP, 
+				this.getClass().getResource("InheritanceTestModelA.inflect"), 
+				this.getClass().getResource("InheritanceTestModelB.inflect"));
+		InflectionCompiler.compile(job);
+		assertFalse(job.getCompilationFaults().isEmpty());
+	}
+	
+	@Test
+	public void testInheritance2() throws Exception {
+		CompilationJob job = CompilerTestUtility.createCompilationJob(CompilationMode.BOOTSTRAP, 
+				this.getClass().getResource("InheritanceTestModelC.inflect"), 
+				this.getClass().getResource("InheritanceTestModelD.inflect"));
+		InflectionCompiler.compile(job);
+		assertFalse(job.getCompilationFaults().isEmpty());
 	}
 	
 }
