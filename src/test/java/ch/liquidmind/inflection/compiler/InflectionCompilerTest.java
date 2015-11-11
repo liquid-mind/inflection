1681378858cdf2711be3278ec55e342c170f0ef3
Adrian Moser <trekking09@gmail.com>
2015-11-11 13:09:38 +0100

    tests: compiler tests improved

--- src/test/java/ch/liquidmind/inflection/compiler/InflectionCompilerTest.java
+++ src/test/java/ch/liquidmind/inflection/compiler/InflectionCompilerTest.java
@@ -1,8 +1,13 @@
 package ch.liquidmind.inflection.compiler;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.net.URL;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.io.Files;
@@ -18,7 +23,20 @@ public class InflectionCompilerTest {
 		File file = new File(url.toURI());
 		CompilationJob job = new CompilationJob(TaxonomyLoader.getSystemTaxonomyLoader(), Files.createTempDir(), CompilationMode.NORMAL, file);
 		InflectionCompiler.compile(job);
-		// TODO asserts
+		assertFalse(job.getCompilationUnits().isEmpty());
+	}
+	
+	@Test
+	@Ignore("IndexOutOfBoundsException is thrown")
+	public void testMessage() throws Exception {
+		URL url = this.getClass().getResource("CompilerTestModel.inflect");
+		File file = new File(url.toURI());
+		CompilationJob job = new CompilationJob(TaxonomyLoader.getSystemTaxonomyLoader(), Files.createTempDir(), CompilationMode.BOOTSTRAP, file);
+		InflectionCompiler.compile(job);
+		assertFalse(job.getCompilationFaults().isEmpty());
+		String message = job.getCompilationFaults().get(0).createFaultMessage();
+		assertNotNull(message != null);
+		assertTrue(message.length() > 0);
 	}
 	
 }
