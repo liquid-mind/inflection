33a3dbbb9908324862c80900b0cafbfba239290e
John Brush <john@johnbrush.ch>
2015-11-13 16:32:29 +0100

    Modified definition of Inflection identifiers to match Java (can contain underscore and/or dollar).

--- src/main/java/ch/liquidmind/inflection/compiler/Pass2Listener.java
+++ src/main/java/ch/liquidmind/inflection/compiler/Pass2Listener.java
@@ -406,6 +406,7 @@ public class Pass2Listener extends AbstractInflectionListener
 		Set< String > matchingClasses = new HashSet< String >();
 		
 		Set< ClassInfo > allClasses = ExceptionWrapper.ClassPath_from( getTaxonomyLoader().getClassLoader() ).getAllClasses();
+		// TODO: classes from rt.jar (bootstrap class loader) are not being included.
 		
 		for ( ClassInfo aClass : allClasses )
 			if ( aClass.getName().matches( packageRegEx ) )
