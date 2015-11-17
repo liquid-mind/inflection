a27f3d4f877107921ff48a44fae24855611573a9
Adrian Moser <trekking09@gmail.com>
2015-11-17 15:49:00 +0100

    ViewTest refactored, naming improved

--- src/test/java/ch/liquidmind/inflection/model/external/ViewTest.java
+++ src/test/java/ch/liquidmind/inflection/model/external/ViewTest.java
@@ -6,39 +6,40 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 import ch.liquidmind.inflection.test.blackbox.BlackboxTestUtility;
-import ch.liquidmind.inflection.test.model.Address;
+import ch.liquidmind.inflection.test.model.A;
+import ch.liquidmind.inflection.test.model.B1;
 
 public class ViewTest {
 
 	@Test
-	public void testGetMember()
+	public void testGetMember_DeclaredMember_MemberExists()
 	{
-		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "ChildTaxonomy");
-		View view = taxonomy.getView(Address.class.getCanonicalName());
-		assertNotNull(view.getMember("street"));
+		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "ViewTest_GetMember_DeclaredMemberTaxonomy");
+		View view = taxonomy.getView(B1.class.getCanonicalName());
+		assertNotNull(view.getMember("longMember"));
 	}
 	
 	@Test
-	public void testGetDeclaredMember()
+	public void testGetDeclaredMember_DeclaredMember_MemberExists()
 	{
-		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "ChildTaxonomy");
-		View view = taxonomy.getView(Address.class.getCanonicalName());
-		assertNotNull(view.getDeclaredMember("street"));
+		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "ViewTest_GetDeclaredMember_DeclaredMemberTaxonomy");
+		View view = taxonomy.getView(B1.class.getCanonicalName());
+		assertNotNull(view.getDeclaredMember("longMember"));
 	}
 	
 	@Test
-	public void testGetSuperMember()
+	public void testMember_InheritedMember_MemberExists()
 	{
-		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "ChildTaxonomy");
-		View view = taxonomy.getView(Address.class.getCanonicalName());
+		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "ViewTest_GetMember_InheritedMemberTaxonomy");
+		View view = taxonomy.getView(A.class.getCanonicalName());
 		assertNotNull(view.getMember("id"));
 	}
 	
 	@Test
-	public void testGetParentView()
+	public void testGetParentTaxonomy_ChildView_ParentExists()
 	{
-		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "ChildTaxonomy");
-		View view = taxonomy.getView(Address.class.getCanonicalName());
+		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "ViewTest_GetParentTaxonomy");
+		View view = taxonomy.getView(B1.class.getCanonicalName());
 		Taxonomy currentParent = view.getParentTaxonomy();
 		assertNotNull(currentParent);
 		assertEquals(taxonomy, currentParent);
