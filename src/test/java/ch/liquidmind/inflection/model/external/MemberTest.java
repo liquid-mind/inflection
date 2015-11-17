862817ee35fd224e0689183b8ddb1f0612e7cb9a
Adrian Moser <trekking09@gmail.com>
2015-11-17 15:59:36 +0100

    MemberTest refactored, naming improved

--- src/test/java/ch/liquidmind/inflection/model/external/MemberTest.java
+++ src/test/java/ch/liquidmind/inflection/model/external/MemberTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 
 import ch.liquidmind.inflection.model.SelectionType;
 import ch.liquidmind.inflection.test.blackbox.BlackboxTestUtility;
-import ch.liquidmind.inflection.test.model.Address;
+import ch.liquidmind.inflection.test.model.A;
 
 public class MemberTest {
 		
 	@Test
-	public void testSelector()
+	public void testGetSelectionType_Include_CorrectType()
 	{
-		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "MemberTaxonomy");
-		View view = taxonomy.getView(Address.class.getCanonicalName());
-		Member member = view.getDeclaredMember("street");
+		Taxonomy taxonomy = BlackboxTestUtility.getTestTaxonomy(this.getClass().getPackage().getName(), "MemberTest_GetSelectionTypeTaxonomy");
+		View view = taxonomy.getView(A.class.getCanonicalName());
+		Member member = view.getDeclaredMember("id");
 		assertEquals(SelectionType.INCLUDE, member.getSelectionType());
 	}
 	
