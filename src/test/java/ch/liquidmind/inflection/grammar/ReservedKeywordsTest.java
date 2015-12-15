66560a8561bfd4697b0ddf146eb953b6379d965f
Adrian Moser <trekking09@gmail.com>
2015-12-15 09:40:48 +0100

    added inflection reserved keywords to reserved keywords test

--- src/test/java/ch/liquidmind/inflection/grammar/ReservedKeywordsTest.java
+++ src/test/java/ch/liquidmind/inflection/grammar/ReservedKeywordsTest.java
@@ -1,7 +1,9 @@
 package ch.liquidmind.inflection.grammar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,13 +17,16 @@ import ch.liquidmind.inflection.test.AbstractInflectionTest;
 public class ReservedKeywordsTest extends AbstractInflectionTest
 {
 
-	private static String[] reservedKeywords = new String[] { "abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while" };
+	private static String[] javaReservedKeywords = new String[] { "abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while" };
+	private static String[] inflectionReservedKeywords = new String[] { "taxonomy", "view", "use", "property", "field", "include", "exclude", "as" };
 
 	@Parameters( name = "{index}: Reserved keyword: {0}" )
 	public static Collection< Object[] > data()
 	{
 		Collection< Object[] > list = new ArrayList< >();
-		for ( String keyword : reservedKeywords )
+		String[] allReservedKeywords = Stream.concat(Arrays.stream(javaReservedKeywords), Arrays.stream(inflectionReservedKeywords)).toArray(String[]::new);
+		
+		for ( String keyword : allReservedKeywords )
 		{
 			list.add( new Object[] { keyword } );
 		}
