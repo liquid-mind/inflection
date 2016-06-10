e602c9dfb2816c5d4e02256779d2f63f26081747
John Brush <john@johnbrush.ch>
2016-06-10 10:32:52 +0200

    Updated test inflection file.

--- src/main/java/ch/liquidmind/inflection/selectors/Selectors.java
+++ src/main/java/ch/liquidmind/inflection/selectors/Selectors.java
@@ -14,7 +14,37 @@ public class Selectors
 	
 	public static boolean matches( String wildcardExpression )
 	{
-		// TODO: implement.
-		return true;
+		boolean matches;
+		SelectorContext selectorContext = SelectorContext.get();
+		String wildcardExpressionRegEx = wildcardExpression.replace( ".", "\\." ).replace( "*", "[a-zA-Z0-9_$]*?" ).toLowerCase();
+		
+		if ( selectorContext instanceof ClassSelectorContext )
+			matches = matches( (ClassSelectorContext)selectorContext, wildcardExpressionRegEx );
+		else if ( selectorContext instanceof PropertySelectorContext )
+			matches = matches( (PropertySelectorContext)selectorContext, wildcardExpressionRegEx );
+		else if ( selectorContext instanceof FieldSelectorContext )
+			matches = matches( (FieldSelectorContext)selectorContext, wildcardExpressionRegEx );
+		else
+			throw new IllegalStateException( "Unexpected type for selectorContext: " + selectorContext.getClass().getName() );
+		
+		return matches;
+	}
+	
+	private static boolean matches( ClassSelectorContext classSelectorContext, String wildcardExpression )
+	{
+		String className = classSelectorContext.getCurrentClass().getName().toLowerCase();
+		return className.matches( wildcardExpression );
+	}
+	
+	private static boolean matches( FieldSelectorContext fieldSelectorContext, String wildcardExpression )
+	{
+		String fieldName = fieldSelectorContext.getCurrentField().getName().toLowerCase();
+		return fieldName.matches( wildcardExpression );
+	}
+	
+	private static boolean matches( PropertySelectorContext propertySelectorContext, String wildcardExpression )
+	{
+		String propertyName = propertySelectorContext.getCurrentProperty().getName().toLowerCase();
+		return propertyName.matches( wildcardExpression );
 	}
 }
